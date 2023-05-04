6bf6e42fdc0f08ad2359df1c31bc25a6d14f54ad
吴再发 <wuzaifa@wuzaifadeMacBook-Pro.local>
2023-05-04 17:04:33 +0800

    feat: 🚀 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: Gauche楽
+ * @Date: 2023-04-18 17:01:18
+ * @LastEditors: Gauche楽
+ * @LastEditTime: 2023-05-03 23:41:28
+ * @FilePath: /Gauche-admin-template/src/App.tsx
+ */
+import logo from "/vite.svg";
+function App() {
+	return (
+		<div className="App">
+			hello word <img src={logo} alt="" />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: Gauche楽
+ * @Date: 2023-04-18 17:01:18
+ * @LastEditors: Gauche楽
+ * @LastEditTime: 2023-05-03 23:37:17
+ * @FilePath: /Gauche-admin-template/src/main.tsx
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
